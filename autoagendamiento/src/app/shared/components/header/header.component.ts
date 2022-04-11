8da90b677bb27fec4398fb94fcc66f36bc1fdb44
avaldivieso <avaldivieso@morris-labs.com>
2022-04-11 18:09:26 -0400

    Cambios en el font

--- autoagendamiento/src/app/shared/components/header/header.component.ts
+++ autoagendamiento/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, } from '@angular/core';
+import { tap } from 'rxjs';
+import { ClientData } from 'src/app/pages/stepper/interfaces/clientData.interface';
+import { ClientDataService } from 'src/app/pages/stepper/service/clientData.service';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +9,16 @@ import { Component, } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  
+  clients!: ClientData[];
+
+  constructor(private clientDataSvc: ClientDataService ) { }
+
+  ngOnInit(): void {
+    this.clientDataSvc.getClientData()
+    .pipe(
+      tap((clients: ClientData[]) => this.clients = clients)
+    )
+    .subscribe();
+  }
  
 }
