02bf0fe579afdef0112b877460140dc953502c88
valdivieso.ant <valdivieso.ant@gmail.com>
2022-10-12 01:15:23 -0300

    Create Docs

--- autoagendamiento/karma.conf.js
+++ autoagendamiento/karma.conf.js
@@ -39,6 +39,9 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    kit: {
+      floc: true
+    }
   });
 };
